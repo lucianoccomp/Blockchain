33d2408070b3e913a36969e0425a7e66d6303dff
lucianoccomp <lucianoccomp@yahoo.com.br>
2025-04-07 17:42:16 -0300

    Adicionado o link do servidor para rodar o código

--- DonateCrypto.sol
+++ DonateCrypto.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+//https://remix.ethereum.org/#lang=en&optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.26+commit.8a97fa7a.js
 
 pragma solidity ^0.8.26;
 
